e973a56fbad7678c1b34be158c0068ebaa917512
Tycho Lin <tycho.lin@gmail.com>
2018-06-13 13:26:50 +0800

    improve crawl_ptt.py

---
+++ README.md
@@ -0,0 +1,2 @@
+Python version: 3.5.3
+pip install requirements.txt
--- crawl_ptt.py
+++ crawl_ptt.py
@@ -3,20 +3,25 @@ import requests
 from bs4 import BeautifulSoup
 from bs4.element import NavigableString
 
-# prefix = 'https://www.ptt.cc{}'
-# url = prefix.format('/bbs/index.html')
-# r = requests.get(url, cookies=dict(over18='1'))
-# soup = BeautifulSoup(r.text, 'html.parser')
-# boards = [e.attrs['href'] for e in soup.find_all('a', attrs={'class': 'board'})]
-
-# board = boards[random.randint(0, len(boards))]
-# url = prefix.format(board)
-# r = requests.get(url, cookies=dict(over18='1'))
-# soup = BeautifulSoup(r.text, 'html.parser')
-
-url = 'https://www.ptt.cc/bbs/Gossiping/M.1527485243.A.FA9.html'
+prefix = 'https://www.ptt.cc{}'
+url = prefix.format('/bbs/index.html')
 r = requests.get(url, cookies=dict(over18='1'))
 soup = BeautifulSoup(r.text, 'html.parser')
+boards = [e.attrs['href'] for e in soup.find_all('a', attrs={'class': 'board'})]
+
+board = boards[random.randint(0, len(boards))]
+url = prefix.format(board)
+r = requests.get(url, cookies=dict(over18='1'))
+soup = BeautifulSoup(r.text, 'html.parser')
+
+articles = [e.find('a') for e in soup.find_all('div', attrs={'class': 'r-ent'})]
+articles = [article.attrs['href'] for article in articles if article]
+
+article = articles[random.randint(0, len(articles))]
+url = prefix.format(article)
+r = requests.get(url, cookies=dict(over18='1'))
+soup = BeautifulSoup(r.text, 'html.parser')
+
 author, board_name, article_title, article_time = (e.string for e in soup.find_all('span', class_='article-meta-value'))
 
 main_content = soup.find(id='main-content')
